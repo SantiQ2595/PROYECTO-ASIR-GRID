985a47c31dce9b960f08f51896edef2de4f82b52
santijose <santiquadros@gmail.com>
2026-03-11 16:58:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+gsap.from(".navbar", {
+  duration: 1,
+  y: -80,
+  opacity: 0,
+});
+
 gsap.from(".main-title", {
   duration: 1,
   y: -50,
   opacity: 0,
+});
+
+gsap.from(".feature-card", {
+  duration: 1,
+  opacity: 0,
+  y: 30,
+  stagger: 0.3,
 });
